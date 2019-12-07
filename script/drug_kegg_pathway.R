c6aa0dcf1199b553974e91c5c609f0d7cfbcc976
Tu Hu <tu@jingyaohu.com>
2019-12-07 19:33:20 +0100

    kegg candidate

--- script/drug_kegg_pathway.R
+++ script/drug_kegg_pathway.R
@@ -1,27 +1,28 @@
 library(tidyverse)
 library(readr)
 library(readxl)
-library(webchem)
-
-drug_info <- read_xlsx("data/drug_info.xlsx",sheet = 2)
-# KEGG <- cts_convert(query = drug_info$`CAS Number`[3],
-#                     from = "CAS", 
-#                     to="KEGG")
-# cts_convert(query = drug_info$`CAS Number`[1],from = "CAS", to="KEGG")
-
-t <- 1:1953
-#query_result <- sapply(t,function(t){
-drug_name <- drug_info$`Product Name`[t]
-query_result <- 
-  keggFind("drug",drug_name %>% 
-                           str_remove(pattern = "\\(.{1,50}\\)") %>% 
-                           str_replace("HCl|2HCl", "hydrochloride"))  %>% 
-                           names() %>% str_remove("dr:") %>% 
-                           keggGet()
-query_result[[1]]$TARGET
+library(KEGGREST)
 
 
-# })
+drug_info <- read_xlsx("data/drug_info.xlsx",sheet = 2) %>% 
+                select(M.w.,Formula,`Catalog Number`,`CAS Number`,SMILES)
+
+candidate <- read_xlsx("drug_screened/down drugs.xlsx") %>% left_join(drug_info)
+
+kegg <- cts_convert(query = candidate$`CAS Number`,from = "CAS", to="KEGG")
+
+t <- 1:length(kegg)
+kegg_combine <- sapply(t, function(t){
+  paste(kegg[[t]],collapse = "_")
+  })  %>% str_remove_all("C\\d{5}_") %>% str_remove_all("C\\d{5}")
+
+candidate_kegg <- candidate %>% mutate(kegg_code = kegg_combine)
+## hand curate
+kegg_combine_demo <- kegg_combine %>% str_remove("_D\\d{5}") %>% unique
+kegg_combine_demo[kegg_combine_demo != "NA" & kegg_combine_demo != ""]
+
+
+kegg_get_demo <-  keggGet(kegg_combine_demo[kegg_combine_demo != "NA" & kegg_combine_demo != ""])
 
 ###
 drug_info$`Product Name`[t] %>% str_remove(pattern = "\\(.{1,50}") %>% str_replace("HCl", "hydrochloride")
